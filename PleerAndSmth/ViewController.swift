e553ce7de52d8faf1ba23fdcf101bf44352f13ae
TsvetkovaAnna <bringmeflowerz@yandex.ru>
2022-06-12 07:09:36 +0300

    Initial Commit

---
+++ PleerAndSmth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PleerAndSmth
+//
+//  Created by Anna Tsvetkova on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
